b82b3fbac390cebc10a41dfc4a54c1b3060e5e0d
Rodrigue Alcazar <rodriguealcazar@gmail.com>
2011-08-20 20:43:17 +0100

    Populating new virtualenv using pip requirements file

--- bootstrap.py
+++ bootstrap.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
+import os.path
 import sys
 import subprocess
+import argparse
 
-DEFAULT_VIRTUALENV_PATH = "../env"
+DEFAULT_VIRTUALENV_PATH = "./env"
 
-def setup_virtualenv(dest_dir=DEFAULT_VIRTUALENV_PATH):
+def setup_virtualenv(dest_dir):
     print "   Installing virtualenv"
     subprocess.call(
         ["pip", "install", "virtualenv"]
@@ -13,17 +15,28 @@ def setup_virtualenv(dest_dir=DEFAULT_VIRTUALENV_PATH):
 
     print "   Setting up new virtualenv in %s" % dest_dir
     subprocess.call(
-        ["virtualenv", "--no-site-packages", dest_dir]
+        ["virtualenv", "--no-site-packages", "--distribute", dest_dir]
     )
 
-def activate_virtualenv(virtual_env=DEFAULT_VIRTUALENV_PATH):
-    if sys.platform == 'win32':
-        activate = "Scripts\\activate.bat"
-    else:
-        activate = 'bin/activate'
+def setup_dependencies(virtualenv):
+    subprocess.call(
+        ["pip", "install", "-E", virtualenv, "-r", "req.txt"]
+    )
 
-    print "   Activating virtualenv in %s" % virtual_env
+def main(args):
+    setup_virtualenv(args.virtualenv)
+    setup_dependencies(args.virtualenv)
 
 
 if __name__ == "__main__":
-    setup_virtualenv()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--virtualenv","-v",
+        metavar="V",
+        help="Path for the virtualenv",
+        default=DEFAULT_VIRTUALENV_PATH,
+        required=False)
+
+    args = parser.parse_args()
+
+    main(args)
